51d9f504997edf24386f66744b59ea6bb2d1381c
nara <parknara0213@gmail.com>
2024-09-20 11:31:14 +0900

    폴더 구조 변경 : FSD 적용

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/router";
+import { router } from "./router";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import PlayGround from "../PlayGround";
+import PlayGround from "../widgets/PlayGround";
 
 export const router = createBrowserRouter([
   {
